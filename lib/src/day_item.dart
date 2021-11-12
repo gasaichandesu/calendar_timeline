b35a05f9d5b0362e24e883e7e4068301ff43bb2d
Anton Ustinoff <a.a.ustinoff@gmail.com>
2021-11-12 11:14:39 +0400

    fix: day item height

--- lib/src/day_item.dart
+++ lib/src/day_item.dart
@@ -54,7 +54,6 @@ class DayItem extends StatelessWidget {
       color: activeDayColor ?? Colors.white,
       fontSize: 32 * scale,
       fontWeight: FontWeight.bold,
-      height: 0.8,
     );
 
     return GestureDetector(
@@ -72,7 +71,7 @@ class DayItem extends StatelessWidget {
             : BoxDecoration(color: Colors.transparent),
         child: Column(
           children: <Widget>[
-            SizedBox(height: (isSelected ? 13.0 : 6.0) * scale),
+            SizedBox(height: (6.0) * scale),
             Text(
               dayNumber.toString(),
               style: isSelected ? selectedStyle : textStyle,
@@ -89,8 +88,8 @@ class DayItem extends StatelessWidget {
                     : dayNameColor ?? Colors.white,
               ),
             ),
-            if (hasEvents && !isDimmed) SizedBox(height: 4.0 * scale),
-            if (hasEvents && !isDimmed) _buildDot(),
+            if (hasEvents) SizedBox(height: 4.0 * scale),
+            if (hasEvents) _buildDot(),
           ],
         ),
       ),
